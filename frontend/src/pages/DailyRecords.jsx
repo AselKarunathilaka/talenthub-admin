dcb278a029dab02812781cee87a06ff24955e0a4
Rukshan Thathsara <136584471+rukshanthathsara@users.noreply.github.com>
2026-06-09 11:15:48 +0530

    Update DailyRecords.jsx

--- frontend/src/pages/DailyRecords.jsx
+++ frontend/src/pages/DailyRecords.jsx
@@ -327,7 +327,7 @@ useEffect(() => {
     
   //Holiday Styling
       if (holiday && !isSelected) {
-    classes += " bg-red-50 border-red-300";
+    classes += " bg-red-60 border-red-300"; 
   }
     if (isToday) classes += " bg-blue-50 border-blue-200";
     if (isSelected) classes += " bg-indigo-100 border-indigo-300 shadow-md";
