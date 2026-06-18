49aa4bdae03da80a6b15c02377633a7d7255053f
Tharushi456 <dimalshacooray@gmail.com>
2026-06-18 23:35:09 +0530

    Updated the session logout of intern side to be 24 hours

--- backend/services/authService.js
+++ backend/services/authService.js
@@ -78,7 +78,7 @@ class AuthService {
     const token = jwt.sign(
       { id: intern._id, email: intern.Trainee_Email },
       dotenv.jwtSecret,
-      { expiresIn: "1h" },
+      { expiresIn: "24h" },
     );
 
     return { token, internId: intern._id, message: "Login successful!" }; // Return internId
@@ -108,7 +108,7 @@ class AuthService {
     const token = jwt.sign(
       { id: intern._id, email: intern.Trainee_Email },
       dotenv.jwtSecret,
-      { expiresIn: "1h" },
+      { expiresIn: "24h" },
     );
 
     return { token, internId: intern._id, message: "Login successful!" };
@@ -137,7 +137,7 @@ class AuthService {
     const token = jwt.sign(
       { id: gateStaff._id, email: gateStaff.email, role: "gatestaff" },
       dotenv.jwtSecret,
-      { expiresIn: "1h" },
+      { expiresIn: "24h" },
     );
 
     return {
@@ -165,7 +165,7 @@ class AuthService {
     const token = jwt.sign(
       { id: newStaff._id, email: newStaff.email, role: "gatestaff" },
       dotenv.jwtSecret,
-      { expiresIn: "1h" },
+      { expiresIn: "24h" },
     );
 
     return {
